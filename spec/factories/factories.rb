de0e472f54f1747e75b71103357bbe09e5d8781d
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-22 09:32:16 +0300

    config factory

---
+++ spec/factories/factories.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :user do
+        email { Faker::Internet.email }
+        password { Faker::Internet.password }
+        password_confirmation { password }
+        confirmed_at { Time.now }
+    end
+end
