a48588ffac7413403225624ac057eed7e5c30e97
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-22 10:11:44 +0300

    Add user

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
-    factory :user do
-        email { Faker::Internet.email }
-        password { Faker::Internet.password }
-        password_confirmation { password }
-        confirmed_at { Time.now }
-    end
+  factory :user do
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    password_confirmation { password }
+    confirmed_at { Time.now }
+  end
 end
