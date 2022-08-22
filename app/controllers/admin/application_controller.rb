36bbaf59743c215e35d150dee6c1fd8f51261fed
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-22 15:08:24 +0300

    Pundit check the permission

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,7 +6,7 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    include Devise::Controllers
+    include Devise::Controllers::Helpers
     before_action :authenticate_admin
 
     def authenticate_admin
