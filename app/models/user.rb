c94fdedc279f774d7ce6d1696cd1e3f5cf8cde10
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-22 11:51:28 +0300

    Attibute a role to user after cretion

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :role
+  after_initialize :set_default_role
+
+  validates :username, presence: true, uniqueness: true
+
+  # def requires_confirmation?
+  #   !confirmed?
+  # end
+
+  def update_role(role_name)
+    self.role = Role.find_or_create_by(name: role_name)
+    save
+  end
+
+
+  private
+
+  def set_default_role
+    self.role ||= Role.find_by(name: 'user') 
+  end
+
+end
